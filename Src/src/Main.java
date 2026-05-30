df85ae026e6367a9ac23821eb82cdff9acf88b79
Yago Araujo <153684752+YagoAr1@users.noreply.github.com>
2026-05-29 21:30:28 -0300

    pastaMain

---
+++ Src/src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+
+        //Importação de classe
+        Conta conta = new Conta();
+
+        //Execução do programa
+        conta.contabancaria();
+    }
+}
